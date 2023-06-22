60a150ca0fbf5d9c9dc53ed7990a0d883aea1367
Jhone Rodrigues <jhonecosta30@gmail.com>
2023-06-22 18:23:14 -0300

    Initial

--- src/model/application/Program.java
+++ src/model/application/Program.java
@@ -3,7 +3,8 @@ package model.application;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world!");
 
 	}
 
