20b67f6f3ec865f72a9dd383e1113a8e01059998
Jhone Rodrigues <jhonecosta30@gmail.com>
2023-07-02 15:20:59 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,8 +32,7 @@ public class Program {
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				
-				System.out.println();
+			
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 				
 				if (capturedPiece != null) {
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -17,6 +17,10 @@ public abstract class ChessPiece extends Piece{
 		return color;
 	}
 	
+	public ChessPosition getChessPosition () {
+		return ChessPosition.fromPosition(position);
+	}
+	
 	protected boolean isThereOponnentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
