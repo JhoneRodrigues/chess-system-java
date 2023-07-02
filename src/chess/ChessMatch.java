8ad4fb4a2b87f4bb4dd48edad88849907b307ad1
Jhone Rodrigues <jhonecosta30@gmail.com>
2023-07-02 14:15:34 -0300

    Handling captured pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -11,6 +14,9 @@ public class ChessMatch {
 	private int turn;
 	private Color currentPlayer;
 	private Board board;
+	
+	private List <Piece> piecesOnTheBoard = new ArrayList<>();
+	private List <Piece> capturedPieces = new ArrayList<>();
 	
 	public ChessMatch(){
 		board = new Board(8, 8);
@@ -53,6 +59,19 @@ public class ChessMatch {
 		return (ChessPiece)capturedPiece;
 	}
 	
+	private Piece makeMove (Position source, Position target) {
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(target);
+		board.placePiece(p, target);
+		
+		if (capturedPiece != null) {
+			piecesOnTheBoard.remove(capturedPiece);
+			capturedPieces.add(capturedPiece);
+		}
+		
+		return capturedPiece;
+	}
+	
 	private void validadeSourcePosition (Position position) {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is not piece on source position");
@@ -65,26 +84,20 @@ public class ChessMatch {
 		}
 	}	
 	
-	private void nextTurn() {
-		turn++;
-		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
-	}
-	
 	private void validadeTargerPosition (Position source, Position target) {
 		if(!board.piece(source).possibleMove(target)) {
 			throw new ChessException("The chosen piece can't move to target position.");
 		}
 	}
 	
-	private Piece makeMove (Position source, Position target) {
-		Piece p = board.removePiece(source);
-		Piece capturePiece = board.removePiece(target);
-		board.placePiece(p, target);
-		return capturePiece;
+	private void nextTurn() {
+		turn++;
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece){
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		piecesOnTheBoard.add(piece);	
 	}
 	
 	private void initialSetup() throws ChessException {
